e169370ad7b3295e59c5455d230e12b9d40a219d
olajvde <eaolaoti@gmail.com>
2022-01-24 17:01:10 +0100

    first

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 const db = mongoose.connection
 
-mongoose.connect('mongodb://localhost/bisibro')
+mongoose.connect('mongodb+srv://bisibro:Capital1@cluster0.tclck.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
 db.on('error', (error) =>console.error(error))
 db.once('open', ()=> console.log('Database connected'))
